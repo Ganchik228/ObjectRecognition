00390bf9915678384911109faf0f733e79ee2949
Ganchik228 <ganchikgamer@gmail.com>
2026-04-29 02:08:45 +0500

    lab3

--- lab3/main.py
+++ lab3/main.py
@@ -1,12 +1,24 @@
-R, B = map(int, input().split())
+from ultralytics import YOLO
 
-S = (R + 4) // 2
-P = B + R
+if __name__ == '__main__':
+    # Новая модель с нуля
+    model_new = YOLO("yolo11n.yaml")
+    model_new.train(data="coco8.yaml", epochs=100, imgsz=640)
 
-discriminant = S * S - 4 * P
-sqrt_disc = int(discriminant ** 0.5)
+    # Предобученная без дообучения
+    model_pretrained = YOLO("yolo11n.pt")
 
-W = (S + sqrt_disc) // 2
-H = (S - sqrt_disc) // 2
+    # Предобученная с дообучение
+    model_finetune = YOLO("yolo11n.pt")
+    model_finetune.train(data="coco8.yaml", epochs=100, imgsz=640)
+   
+   
+    
+    metrics1 = model_new.val(data="coco8.yaml")
+    metrics2 = model_pretrained.val(data="coco8.yaml")
+    metrics3 = model_finetune.val(data="coco8.yaml")
 
-print(W, H)
+    print(f"Новая с нуля:                  mAP50 = {metrics1.box.map50:.3f}")
+    print(f"Предобученная без дообучения:  mAP50 = {metrics2.box.map50:.3f}")
+    print(f"Предобученная + дообучение:    mAP50 = {metrics3.box.map50:.3f}")
+
